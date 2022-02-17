a2eee8fcce87bc3213737e4a8e52475159796a0a
Michela-DC <micheladelconte@outlook.com>
2022-02-17 16:43:59 +0100

    Added functions: bombGenerator and isBomb

--- script.js
+++ script.js
@@ -10,20 +10,13 @@ const gameContainer = document.querySelector('.game-container');
 const message = document.querySelector('.message');
 const points = document.querySelector('.points');
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-}
-
-
 // A seconda del livello si crea una griglia con numero di caselle diverso quindi:
 // di base devo creare un ciclo for per far generare le caselle poi a seconda del livello il ciclo prenderà valori diversi, ovvero quante volte cicla e la width delle celle cambia a seconda del numero di colonne
 const startGame = () => { //creo una funzione dove a seconda del caso si creaono griglie diverse
     // dichiaro le variabili per le caselle e le colonne che cambieranno a seconda del livello
 
-    let squares, columns; 
-    let numbers;
+    let squares, columns, bombs;
+
     // creo i casi a seconda del livello
     switch (levels.value){
         case 'easy':
@@ -33,14 +26,6 @@ const startGame = () => { //creo una funzione dove a seconda del caso si creaono
             columns = 10;
             //salco dentro un array i numeri random che genero
             numbers = [];
-            do{
-                const randomNumber = getRandomIntInclusive(1, squares);
-                //controllo che i numeri on siano doppi dentro l'array
-                if( !numbers.includes(randomNumber) ){ 
-                    numbers.push(randomNumber);
-                }
-            }while (numbers.length < 16)
-            console.log(numbers);
 
             main.style.backgroundColor = '#7FFFD4'
             console.log(gameContainer);
@@ -51,30 +36,15 @@ const startGame = () => { //creo una funzione dove a seconda del caso si creaono
             squares = 81;
             columns = 9;
             numbers = [];
-            do{
-                const randomNumber = getRandomIntInclusive(1, squares);
-                if( !numbers.includes(randomNumber) ){ 
-                    numbers.push(randomNumber);
-                }
-            }while (numbers.length < 16)
-            console.log(numbers);
 
-            main.style.backgroundColor = "gold";
             console.log(gameContainer);
+            main.style.backgroundColor = "gold";
             break;
 
         case 'crazy':
             console.log('level crazy');
             squares = 49;
             columns = 7;
-            numbers = [];
-            do{
-                const randomNumber = getRandomIntInclusive(1, squares);
-                if( !numbers.includes(randomNumber) ){ 
-                    numbers.push(randomNumber);
-                }
-            }while (numbers.length < 16)
-            console.log(numbers);
 
             console.log(gameContainer);
             main.style.backgroundColor = "#FF6347";
@@ -84,24 +54,17 @@ const startGame = () => { //creo una funzione dove a seconda del caso si creaono
     // questa riga di codice fa in modo che il gameContainer, una volta scelto il livello, prima venga svuotato e poi dopo con il for ci ricreo dentro un'altra griglia
     gameContainer.innerHTML = ' ';
 
-    let count = 0;
-
     // funzione per selezionare le celle e per non poterle più riselezionare
     function selectBox() {
         console.log(this.innerHTML);
         const clickedBox = this; //il this si riferisce alla cella
         const clickedNumber = parseInt(this.innerHTML); //aggiungendo .innerHTML mi riferisco al valore dentro la cella e devo fare il parseInt per trasformarlo da stringa in numero
-        
-        gameContainer.onclick = function () {
-            count++;
-            console.log(count);
-        }
 
-        if (numbers.includes(clickedNumber)){
+        //verifico se il numero cliccato è una bomba usando la funzione isBomb che mi sono creata apposta
+        if ( isBomb(clickedNumber, bombs) ){ 
             console.log('bomba!')
             clickedBox.classList.add('bomb-box');
-            message.style.display = 'flex';
-            points.innerHTML += `${count}`;
+            // message.style.display = 'flex';
 
         } else{
             clickedBox.classList.add('selected-box');
@@ -111,9 +74,7 @@ const startGame = () => { //creo una funzione dove a seconda del caso si creaono
         clickedBox.removeEventListener('click', selectBox);
     }
 
- 
-
-    //Una volta che viene scelto il caso si possono creare le celle con un ciclo for e le inserisco dentro il game container
+    // ciclo for la creazione delle celle
     for(let i = 1; i <= squares; i++){
         const box = document.createElement('div');
         box.classList.add('box');
@@ -121,12 +82,45 @@ const startGame = () => { //creo una funzione dove a seconda del caso si creaono
         box.append(i);
         gameContainer.append(box);
 
-
-        // ho semplificato l'addEventListener creando la funzione selectBox (e nel css ho creato una classe apposta per le caselle selzionate)
-        box.addEventListener('click', selectBox); 
-
-        
+        // ogni volta che clicco aggancio una casella usando il this
+        box.addEventListener('click', selectBox);
     }
+
+    bombs = bombGenerator(16,1,squares);
+
+    // funzione per la generazione di numeri random
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+    }
+
+    // creo una funzione per la generazione random dei numeri corrispondenti alle bombe
+    function bombGenerator(bombsNum, min, max){
+        const  array = [];
+
+        do{
+            const randomNumber = getRandomIntInclusive(min, max);
+            if( !array.includes(randomNumber) ){ 
+                array.push(randomNumber);
+            }
+        }while (array.length < bombsNum)
+
+        console.log(array);
+
+        return array;
+    }
+
+    //funzione per verificare se è una bomba
+    function isBomb(num, bombs){
+        if( bombs.includes(parseInt(num)) ){
+            return true;
+        
+        } else {
+            return false;
+        }
+    }
+
 }
 
 playBtn.addEventListener('click',startGame);
